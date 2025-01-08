ba8f9a9d5ac1bbd0e134f81851b998c0f4a1cd67
byronnDev <mikel.e.g.2004@gmail.com>
2025-01-08 19:00:12 +0100

    Actualizar enlaces en la sección "Powered By" y mejorar la traducción en el archivo de traducciones

--- translations.js
+++ translations.js
@@ -3,7 +3,7 @@ export const translations = {
     en: {
         title: "MarkDownAnything",
         lead: "Upload any file and see the result in markdown format to copy and paste into your favorite text editor.",
-        poweredBy: "mikeldev.com",
+        poweredBy: "Powered by <a href=\"https://www.mikeldev.com\">mikeldev</a>",
         dropZoneText: "Drag and drop files here or",
         selectFileButton: "Select file",
         resultTitle: "Result",
@@ -12,7 +12,7 @@ export const translations = {
     es: {
         title: "MarkDownAnything",
         lead: "Sube cualquier archivo y verás el resultado en formato markdown para que puedas copiar y pegarlo en tu editor de texto favorito.",
-        poweredBy: "mikeldev.com",
+        poweredBy: "Desarrollado por <a href=\"https://www.mikeldev.com\">mikeldev</a>",
         dropZoneText: "Arrastra y suelta archivos aquí o",
         selectFileButton: "Seleccionar archivo",
         resultTitle: "Resultado",
