fa8a3104b8859375b8b2361c2a607d4e3b671492
byronnDev <mikel.e.g.2004@gmail.com>
2025-01-08 18:52:15 +0100

    Agregar sección "Powered By" y soporte para traducciones en inglés y español

---
+++ translations.js
@@ -0,0 +1,21 @@
+
+export const translations = {
+    en: {
+        title: "MarkDownAnything",
+        lead: "Upload any file and see the result in markdown format to copy and paste into your favorite text editor.",
+        poweredBy: "mikeldev.com",
+        dropZoneText: "Drag and drop files here or",
+        selectFileButton: "Select file",
+        resultTitle: "Result",
+        copyButtonTitle: "Copy"
+    },
+    es: {
+        title: "MarkDownAnything",
+        lead: "Sube cualquier archivo y verás el resultado en formato markdown para que puedas copiar y pegarlo en tu editor de texto favorito.",
+        poweredBy: "mikeldev.com",
+        dropZoneText: "Arrastra y suelta archivos aquí o",
+        selectFileButton: "Seleccionar archivo",
+        resultTitle: "Resultado",
+        copyButtonTitle: "Copiar"
+    }
+};
